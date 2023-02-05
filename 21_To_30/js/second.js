8c68760d755d1a0ff2642c279be87824be44224a
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-02-05 19:42:34 +0500

    Assignments of Chapters 21 To 30 Completed

--- 21_To_30/js/second.js
+++ 21_To_30/js/second.js
@@ -2,4 +2,4 @@ let inputValue = prompt("Please Enter a Phrase: ").toLowerCase();
 
 inputValue = inputValue.split(" ").map(item => item[0].toUpperCase() + item.slice(1)).join(" ");
 
-alert(inputValue);
+console.log(inputValue);
