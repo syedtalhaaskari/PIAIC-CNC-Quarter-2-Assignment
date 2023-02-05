3bfe0b0c93f98687b296fbcd961e05e1f975caba
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-02-05 19:12:05 +0500

    Assignments of Chapters 15, 16 and 17 Completed

--- 15_16_17/js/app.js
+++ 15_16_17/js/app.js
@@ -1,13 +1,27 @@
 let courses = [];
 
-for (let i = 0; i < 5; i++) {
-    courses[i] = prompt(`Please Enter Course ${i + 1} Name: `, "");
-}
+let course1 = prompt("Please Enter Course 1 Name", "computer");
+courses.push(course1);
+let course2 = prompt("Please Enter Course 2 Name", "computer");
+courses.push(course2);
+let course3 = prompt("Please Enter Course 3 Name", "computer");
+courses.push(course3);
+let course4 = prompt("Please Enter Course 4 Name", "computer");
+courses.push(course4);
+let course5 = prompt("Please Enter Course 5 Name", "computer");
+courses.push(course5);
 
 alert(courses);
 
-for (let i = 0; i < 5; i++) {
-    courses[i] = prompt(courses[i], courses[i]);
-}
+let course1ValueEdit = prompt(courses[0], courses[0]);
+courses.splice(0, 1, course1ValueEdit);
+let course2ValueEdit = prompt(courses[1], courses[1]);
+courses.splice(1, 1, course2ValueEdit);
+let course3ValueEdit = prompt(courses[2], courses[2]);
+courses.splice(2, 1, course3ValueEdit);
+let course4ValueEdit = prompt(courses[3], courses[3]);
+courses.splice(3, 1, course4ValueEdit);
+let course5ValueEdit = prompt(courses[4], courses[4]);
+courses.splice(4, 1, course5ValueEdit);
 
 alert(courses);
