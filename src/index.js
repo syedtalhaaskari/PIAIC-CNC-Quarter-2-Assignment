503485f1691bfd0b70b4eea2adb0358d191f647e
syedtalhaaskari <88083531+syedtalhaaskari97@users.noreply.github.com>
2023-02-24 19:39:15 +0500

    Create a React Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <h1>Rick &amp; Morty API</h1>
+    )
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
